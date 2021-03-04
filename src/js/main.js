d59417e668c9a24c4e1b99f6273ab5ea4e7ecc9f
Minaev Vlad <uiNoiSE@gmail.com>
2021-03-04 18:49:45 +0300

    + full desktop queries + parallax in "composition" + mobile animations in "composition" -- final commit --

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,28 @@ window.addEventListener("load", pageReady, false);
 
 function pageReady(e) {
   document.querySelector(".splash__logo").classList.add("hide");
-  setTimeout(document.querySelector(".splash > .container").classList.remove("disabled"), 1000)
+  setTimeout(
+    document.querySelector(".splash > .container").classList.remove("disabled"),
+    1000
+  );
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (isMobile) {
+    console.log("");
+    
+  } else {
+    function parallax(event) {
+      setTimeout(() => {
+        this.querySelectorAll(".js-layer").forEach((layer) => {
+          let speed = layer.getAttribute("data-speed");
+          layer.style.transform = `translate(${
+            (event.clientX * speed) / 500
+          }px, ${(event.clientY * speed) / 500}px)`;
+        });
+      }, 100);
+    }
+    document.addEventListener("mousemove", parallax);
+  }
+});
